669d6057487f0f7546d5cb2f66006816153112c8
Jeremy Duncan <jduncan46@my.devry.edu>
2022-08-29 09:13:16 -0400

    added comments

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -3,12 +3,16 @@ public class EmployeeTest
     public static void main(String[] args)
     {
         System.out.println("Testing Employee class");
+
         // instantiate an object (create a cookie)
         Employee emp = new Employee();
+
+        // assign a value for name, rate, and hours
         emp.setName("John Smith");
-        emp.setRate(25.99);
+        emp.setRate(10);
         emp.setHours(40);
 
+        // verifiy by printing the value
         System.out.println("Name is " + emp.getName());
         System.out.println(emp.getHours()+ " hours a week.");
         System.out.println("$"+ emp.getRate()+ " an hour.");
